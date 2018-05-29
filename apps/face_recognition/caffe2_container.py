83f046f6c70c73df09d0c79acd2122933097f477
v-yucj <v-yucj@microsoft.com>
2018-05-28 23:09:32 -0700

    Add face recognition app

---
+++ apps/face_recognition/caffe2_container.py
@@ -0,0 +1,123 @@
+from __future__ import print_function
+import rpc
+import os
+import sys
+import glob
+import caffe2
+from caffe2.python import core, workspace
+from caffe2.proto import caffe2_pb2
+import numpy as np
+import skimage.io
+import skimage.transform
+import base64
+
+
+def predict_function(net_def, device_opts, imgs):
+    NCHW_batch = np.zeros((len(imgs),3,224,224))
+
+    for i, curr_img in enumerate(imgs):
+        img = skimage.img_as_float(skimage.io.imread(curr_img)).astype(np.float32)
+        img = rescale(img, 224, 224)
+        img = crop_center(img, 224, 224)
+        img = img.swapaxes(1, 2).swapaxes(0, 1)
+        img = img[(2, 1, 0), :, :]
+        img = img * 255 - mean
+        NCHW_batch[i] = img
+
+    workspace.FeedBlob('data', NCHW_batch.astype(np.float32), device_opts)
+    workspace.RunNet(net_def.name, 1)
+
+
+class caffe2Container(rpc.ModelContainerBase):
+    def __init__(self, path, input_type, gpu_id):
+        self.device_opts = core.DeviceOption(caffe2_pb2.CUDA, gpu_id)
+
+        self.input_type = rpc.string_to_input_type(input_type)
+        modules_folder_path = "{dir}/".format(dir=path)
+        INIT_NET = "{dir}/init_net.pb".format(dir=modules_folder_path)
+        PREDICT_NET = "{dir}/predict_net.pb".format(dir=modules_folder_path)
+        self.predict_func = predict_function
+
+        init_def = caffe2_pb2.NetDef()
+        with open(INIT_NET, 'r') as f:
+            init_def.ParseFromString(f.read())
+            init_def.device_option.CopyFrom(self.device_opts)
+            workspace.RunNetOnce(init_def)
+
+        self.net_def = caffe2_pb2.NetDef()
+        with open(PREDICT_NET, 'r') as f:
+            self.net_def.ParseFromString(f.read())
+            self.net_def.device_option.CopyFrom(self.device_opts)
+            workspace.CreateNet(self.net_def, overwrite=True)
+
+
+    def predict_strings(self, inputs):
+        imgs = []
+        for i in range(len(inputs)):
+            img_bgr = base64.b64decode(inputs[i])
+            imgs.append(img_bgr)
+
+        preds = self.predict_func(self.net_def, self.device_opts, imgs)
+    
+    #def predict_bytes(self, inputs):
+    #    imgs = []
+    #    for i in range(len(inputs)):
+    #        img_bgr = cv2.imdecode(inputs[i], cv2.CV_LOAD_IMAGE_COLOR)
+    #        imgs.append(img_bgr)
+
+    #    preds = self.predict_func(self.net_def, self.device_opts, imgs)
+
+    
+if __name__ == "__main__":
+    print("Starting Caffe2 container")
+    try:
+        model_name = os.environ["CLIPPER_MODEL_NAME"]
+    except KeyError:
+        print(
+            "ERROR: CLIPPER_MODEL_NAME environment variable must be set",
+            file=sys.stdout)
+        sys.exit(1)
+
+    try:
+        model_version = os.environ["CLIPPER_MODEL_VERSION"]
+    except KeyError:
+        print(
+            "ERROR: CLIPPER_MODEL_VERSION environment variable must be set",
+            file=sys.stdout)
+        sys.exit(1)
+
+    #ip = "localhost"
+    if "CLIPPER_IP" in os.environ:
+        ip = os.environ["CLIPPER_IP"]
+    else:
+        print(
+            "ERROR: CLIPPER_IP environment variable must be set",
+            file=sys.stdout)
+        sys.exit(1)
+
+    port = 7000
+    if "CLIPPER_PORT" in os.environ:
+        port = int(os.environ["CLIPPER_PORT"])
+    else:
+        print("Connecting to Clipper with default port: 7000")
+
+    input_type = "strings"
+    if "CLIPPER_INPUT_TYPE" in os.environ:
+        input_type = os.environ["CLIPPER_INPUT_TYPE"]
+    else:
+        print("Using default input type: strings")
+
+    if "CLIPPER_MODEL_PATH" in os.environ:
+        path = str(os.environ["CLIPPER_MODEL_PATH"])
+    else:
+        print("Clipper model path not found.")
+
+    if "GPU_ID" in os.environ:
+        gpu_id = int(os.environ["GPU_ID"])
+    else:
+        print("GPU id not set")
+
+    print("Init model")
+    model = caffe2Container(path, input_type, gpu_id)
+    rpc_service = rpc.RPCService()
+    rpc_service.start(model, ip, port, model_name, model_version, input_type)
---
+++ apps/face_recognition/container_entry.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+CONTAINER_SCRIPT_PATH=$1
+IMPORT_ERROR_RETURN_CODE=3
+
+export CLIPPER_MODEL_NAME="vgg-face"
+export CLIPPER_MODEL_VERSION="1"
+export CLIPPER_MODEL_PATH="/hdfs/pnrsy/v-haicsh/nexus-models/store/caffe2/vgg_face/1"
+export CLIPPER_IP="10.0.0.70" #IP address of the frontend container
+export CLIPPER_PORT="7000" 
+export GPU_ID=$2
+echo $GPU_ID
+
+/bin/bash -c "exec python $CONTAINER_SCRIPT_PATH"
+
+if [ $? -eq $IMPORT_ERROR_RETURN_CODE ]; then
+  echo "Encountered an ImportError when running container. You can use the pkgs_to_install argument when calling clipper_admin.build_model() to supply any needed Python packages."
+  exit 1
+fi
+
+echo "Encountered error not related to missing packages. Please refer to the container log to diagnose."
+exit 1
