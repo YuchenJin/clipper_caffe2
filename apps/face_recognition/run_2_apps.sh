16ef23a9c0f3fdd0a793f0b391098f2146d11919
YuchenJin <yuchenj@cs.washington.edu>
2018-06-15 07:02:39 +0000

    Update data and model locations on philly

--- apps/face_recognition/run_2_apps.sh
+++ apps/face_recognition/run_2_apps.sh
@@ -5,7 +5,7 @@
 
 for RATE in 10 20 30 40 50 60 70 80 90 100
 do 
-    for SLA in 50 100 200
+    for SLA in 50
     do 
 	echo "sla: $SLA, rate: $RATE"
         /bin/bash -c "exec python runapp1.py $SLA $RATE" &
--- apps/face_recognition/runapp1.py
+++ apps/face_recognition/runapp1.py
@@ -4,14 +4,14 @@ from clipper_client1 import *
 
 
 def main():
-    sla = int(sys.argv[1])
-    rate = int(sys.argv[2])
+    sla = float(sys.argv[1])
+    rate = float(sys.argv[2])
 
     FORMAT = "[%(asctime)-15s %(levelname)s] %(message)s"
     logging.basicConfig(format=FORMAT)
     logging.getLogger().setLevel(logging.INFO)
 
-    dataset = Dataset('/hdfs/pnrsy/v-haicsh/datasets/vgg_face/')
+    dataset = Dataset('./resized_images/')
     output = 'logs/vgg_face1_sla{}_rate{}.txt'.format(sla, rate)
     test = ThroughputTest(dataset, output)
     duration = 100
