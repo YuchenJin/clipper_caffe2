7d371c61ee47afa5a9aea0622f8f2ba7f3170d35
YuchenJin <yuchenj@cs.washington.edu>
2018-07-06 23:31:48 +0000

    x

--- apps/lenet/runtest.py
+++ apps/lenet/runtest.py
@@ -10,16 +10,15 @@ def run_test(datapath, rate, duration, output, app_id):
     gen.output_latencies(output)
 
 def eval_face(sla, n):
-    def run(rps):
+    def run(rps, iteration):
         print('Test rps %s' % rps)
         threads = []
         outputs = []
         for i in range(n):
-            output = 'logs/lenet{}_sla{}_rate{}.txt'.format(n, i+1, sla, rps)
-	    if sla == 50:
+            output = 'logs/lenet1_iter{}_{}_sla{}_rate{}.txt'.format(iteration, i+1, sla, rps)
+   	    rps = rps * 4.0961935960077902 * 6
+	    if sla == 100:
 	        app_id_base = 1
-	    elif sla == 100:
-	        app_id_base = 5
 	    elif sla == 200:
 	        app_id_base = 9
 	    elif sla == 1000:
@@ -47,24 +46,24 @@ def eval_face(sla, n):
             return False
         return True
         
-    duration = 10
+    duration = 50
     datapath = './resized_images/'
     print(datapath)
     print('Latency sla: %s ms' % sla)
     print("Number of models: %s" % n)
     #run(rps)
 
-    rps = 50 
-    run(rps)
-    #while True:
-    #    good = run(rps)
-    #    if not good:
-    #        break
-    #    rps += 10
-    #for rps in np.arange(rps-9.5, rps, 0.5):
-    #    good = run(rps)
-    #    if not good:
-    #        break
+    rps = 20
+    while True:
+	for i in range(10):
+	    good = run(rps, i)
+	    if good:
+	        break
+        rps += 10
+    for rps in np.arange(rps-9.5, rps, 0.5):
+        good = run(rps, 1)
+        if not good:
+            break
 
 def parse_result(fn):
     total, good = 0, 0
@@ -84,8 +83,8 @@ def main():
     logging.basicConfig(format=FORMAT)
     logging.getLogger().setLevel(logging.INFO)
     #eval_face(200, 2)
-    eval_face(50, 1)
-    #eval_face(100, 1)
+    #eval_face(50, 1)
+    eval_face(100, 1)
     #eval_face(1000, 1)
     #eval_face(1000, 1)
     #eval_face(50, 2)
