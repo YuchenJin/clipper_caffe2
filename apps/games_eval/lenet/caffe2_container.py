b75cd9b7471ab380ea01f443c1d5935ab809a8e8
YuchenJin <yuchenj@cs.washington.edu>
2018-09-12 22:36:18 +0000

    Add game eval

---
+++ apps/games_eval/lenet/caffe2_container.py
@@ -0,0 +1,96 @@
+from __future__ import print_function
+import rpc
+import os
+import sys
+import glob
+import caffe2
+from caffe2.python import core, workspace
+from caffe2.proto import caffe2_pb2
+import numpy as np
+import skimage.io
+import skimage.transform
+import base64
+from StringIO import StringIO
+import json
+
+
+def predict_function(net_def, device_opts, imgs):
+    NCHW_batch = np.zeros((len(imgs),3,28,28))
+
+    for i, curr_img in enumerate(imgs):
+	curr_img = skimage.io.imread(StringIO(curr_img))
+        img = skimage.img_as_float(curr_img).astype(np.float32)
+        img = img.swapaxes(1, 2).swapaxes(0, 1)
+        img = img[(2, 1, 0), :, :]
+        img = img * 255 - 128
+        NCHW_batch[i] = img
+
+    workspace.FeedBlob('data', NCHW_batch.astype(np.float32), device_opts)
+    workspace.RunNet(net_def.name, 1)
+    results = workspace.FetchBlob('prob')
+    return results
+
+
+class caffe2Container(rpc.ModelContainerBase):
+    def __init__(self, path, input_type, gpu_id):
+        self.device_opts = core.DeviceOption(caffe2_pb2.CUDA, gpu_id)
+
+        self.input_type = rpc.string_to_input_type(input_type)
+        modules_folder_path = "{dir}/".format(dir=path)
+        INIT_NET = "{dir}/init_net.pb".format(dir=modules_folder_path)
+        PREDICT_NET = "{dir}/predict_net.pb".format(dir=modules_folder_path)
+	
+        self.predict_func = predict_function
+
+        init_def = caffe2_pb2.NetDef()
+        with open(INIT_NET, 'r') as f:
+            init_def.ParseFromString(f.read())
+            init_def.device_option.CopyFrom(self.device_opts)
+            workspace.RunNetOnce(init_def)
+
+        self.net_def = caffe2_pb2.NetDef()
+        with open(PREDICT_NET, 'r') as f:
+            self.net_def.ParseFromString(f.read())
+            self.net_def.device_option.CopyFrom(self.device_opts)
+            workspace.CreateNet(self.net_def, overwrite=True)
+
+	with open('resized_images/frame407.jpg', 'rb') as f: 
+	    self.predict_strings([base64.b64encode(f.read())])
+
+    def predict_strings(self, inputs):
+        imgs = []
+        for i in range(len(inputs)):
+            img_bgr = base64.b64decode(inputs[i])
+            imgs.append(img_bgr)
+        preds = self.predict_func(self.net_def, self.device_opts, imgs)
+	
+	result = []
+	for i in range(len(preds)):
+	    #result.append(preds[i])
+	    #result.append(preds[i].index(max(preds[i])))
+	    result.append(str(np.argmax(preds[i])))
+   
+	return result
+    
+    
+if __name__ == "__main__":
+    print("Starting Caffe2 container")
+
+    if "GPU_ID" in os.environ:
+        gpu_id = int(os.environ["GPU_ID"])
+    else:
+        print("GPU id not set")
+
+    print("Init model")
+
+    rpc_service = rpc.RPCService()
+    try:
+        model = caffe2Container(rpc_service.get_model_path(),
+                                rpc_service.get_input_type(),
+				gpu_id)
+        sys.stdout.flush()
+        sys.stderr.flush()
+    except ImportError:
+        sys.exit(IMPORT_ERROR_RETURN_CODE)
+    rpc_service.start(model)
+
---
+++ apps/games_eval/tensorflow_resnet/runtest.py
@@ -0,0 +1,86 @@
+import os
+import sys
+from generator import *
+
+
+def run_test(datapath, rate, duration, output, app_id):
+    dataset = Dataset(datapath)
+    gen = Generator(dataset, output, app_id)
+    gen.run(rate, duration)
+    gen.output_latencies(output)
+
+def eval_inception(sla, n):
+    def run(rps, iteration):
+        print('Test rps %s' % rps)
+        threads = []
+        outputs = []
+        for i in range(int(n)):
+            output = 'logs/resnet{}_iter{}_sla{}_rate{}.txt'.format(i+1, iteration, sla, rps)
+	    if sla == 50:
+	        app_id_base = 1
+	    elif sla == 200:
+	        app_id_base = 9
+	    elif sla == 1000:
+	        app_id_base = 13
+	    else:
+		print("Invalid sla")
+		sys.exit()
+            t = Thread(target=run_test, args=(datapath, rps, duration, output, str(i+app_id_base)))
+	    t.daemon = True
+            threads.append(t)
+            outputs.append(output)
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+	aggr_good = 0
+	aggr_total = 0
+	for i, output in enumerate(outputs):
+            good, total = parse_result(output)
+	    percent = float(good) / total
+            print('App %s: %.2f%%' % (i+1, percent*100))
+            aggr_good += good
+            aggr_total += total
+        if float(aggr_good) / aggr_total < .99:
+            return False
+        return True
+        
+    duration = 50
+    datapath = './resized_images/'
+    print(datapath)
+    print('Latency sla: %s ms' % sla)
+    print("Number of models: %s" % n)
+    #run(rps)
+
+    rps = 70
+    while True:
+	for i in range(3):
+	    good = run(rps, i)
+	    if good:
+	        break
+	if good:
+            rps += 20
+	else:
+	    break
+    for rps in np.arange(rps-9.5, rps, 0.5):
+        good = run(rps, 1)
+        if not good:
+            break
+
+def parse_result(fn):
+    total, good = 0, 0
+    with open(fn) as f:
+        for line in f:
+	    total += 1
+	    if "False" in line:
+	        good += 1
+    return good, total
+
+def main():
+    FORMAT = "[%(asctime)-15s %(levelname)s] %(message)s"
+    logging.basicConfig(format=FORMAT)
+    logging.getLogger().setLevel(logging.INFO)
+    eval_inception(int(sys.argv[1]), int(sys.argv[2]))
+
+if __name__ == "__main__":
+    main()
